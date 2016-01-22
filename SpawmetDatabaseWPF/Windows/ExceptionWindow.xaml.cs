17c929d847f7b8eb208e8ad5ba6ad354256483b8
ninjah187 <ninjah187@gmail.com>
2016-01-22 18:39:22 +0100

    error raport sending added

--- SpawmetDatabaseWPF/Windows/ExceptionWindow.xaml.cs
+++ SpawmetDatabaseWPF/Windows/ExceptionWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +22,14 @@ namespace SpawmetDatabaseWPF.Windows.Common
     /// </summary>
     public partial class ExceptionWindow : Window
     {
+        private Exception _exception;
+        private string _message;
+
         private ExceptionWindow(Exception exception)
         {
             InitializeComponent();
+
+            _exception = exception;
 
             var exc = exception;
             bool firstRun = true;
@@ -48,6 +56,8 @@ namespace SpawmetDatabaseWPF.Windows.Common
 
                 exc = exc.InnerException;
             }
+
+            _message = ExceptionInfoTextBox.Text;
         }
 
         public static void Show(Exception exception, Window owner = null)
@@ -70,7 +80,43 @@ namespace SpawmetDatabaseWPF.Windows.Common
 
         protected override void OnClosed(EventArgs e)
         {
+            base.OnClosed(e);
             Application.Current.Shutdown();
+        }
+
+        private async void SendRaportButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            IsEnabled = false;
+            await Task.Run(() =>
+            {
+                var fromAddress = new MailAddress("ninjah187@gmail.com");
+                var toAddress = new MailAddress("ninjah187@gmail.com");
+                var fromPassword = ConfigurationManager.AppSettings["ConfirmationMailPassword"];
+
+                var smtp = new SmtpClient()
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                };
+
+                var message = new MailMessage(fromAddress, toAddress);
+                string msgSubject = "Exception raport " + DateTime.Now.ToString("yyyy-MM-dd HH-mm");
+                string msgBody = _message;
+                message.Subject = msgSubject;
+                message.Body = msgBody;
+
+                smtp.Send(message);
+
+                smtp.Dispose();
+                message.Dispose();
+            });
+            IsEnabled = true;
+
+            Close();
         }
     }
 }
